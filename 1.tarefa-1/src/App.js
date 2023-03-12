b6aefae0e94946f089bce8b418bfb540a85ed429
Flávio Félix <fau_felix30@hotmail.com>
2023-03-11 23:57:11 -0300

    Feito o primeiro desafio

---
+++ 1.tarefa-1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!</h1>
+      <h2>Emment funcionou</h2>
+      <p>Meu primeiro parágrafo</p>
+    </div>
+  );
+}
+
+export default App;
