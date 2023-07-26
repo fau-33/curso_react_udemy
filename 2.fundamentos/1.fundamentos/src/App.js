bb11ce10ffd59ead9529906155b8388a912dc4a8
Flávio Félix <fau_felix30@hotmail.com>
2023-07-26 16:37:17 -0300

    Feito o fundamentos parte 1

--- 2.fundamentos/1.fundamentos/src/App.js
+++ 2.fundamentos/1.fundamentos/src/App.js
@@ -1,11 +1,17 @@
-import FirstComponent from './components/FirstComponent';
 import './App.css';
+import Events from './components/Events';
+import FirstComponent from './components/FirstComponent';
+import MyComponent from './components/MyComponent';
+import TemplateExpressions from './components/TemplateExpressions';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <FirstComponent />
+      <TemplateExpressions/>
+      <MyComponent/>
+      <Events/>
     </div>
   );
 }
--- 2.fundamentos/1.fundamentos/src/components/FirstComponent.js
+++ 2.fundamentos/1.fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,16 @@
+import MyComponent from "./MyComponent";
+
 const FirstComponent = () => {
+  // função
+  /**
+   * multi line
+   */
   return(
     <div>
+      {/** algum comentário */}
         <h1>Meu primeiro componente</h1>
         <p className="teste">meu texto</p>
+        <MyComponent/>
     </div>
   );
 }
