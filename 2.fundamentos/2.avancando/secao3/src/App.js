4ac85cd96eb65c2b2df9b942b67c8b1db8f60332
Flávio Félix <fau_felix30@hotmail.com>
2023-09-27 19:54:48 -0300

    Aulas subindo imagens no public e assets

--- 2.fundamentos/2.avancando/secao3/src/App.js
+++ 2.fundamentos/2.avancando/secao3/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import City from './assets/city.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando no React</h1>
+      {/** Imagem em Public */}
+      <div>
+        <img src='/img1.jpg' alt='Paisagem'/>
+      </div>
+      {/** Imagem em asset */}
+      <div>
+        <img src={City} alt='Cidade'/>
+      </div>
     </div>
   );
 }
