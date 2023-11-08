357da7f3d816b7db675b6bff3cb612be110c2978
Flávio Félix <fau_felix30@hotmail.com>
2023-11-08 14:10:35 -0300

    Feito o condicional e introdução aos props

--- 2.fundamentos/2.avancando/secao3/src/App.js
+++ 2.fundamentos/2.avancando/secao3/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import City from './assets/city.jpg';
+import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  // const name = "Joaquim";
+  const [userName] = useState("Maria");
+
   return (
     <div className="App">
       <h1>Avançando no React</h1>
@@ -17,6 +23,8 @@ function App() {
       </div>
       <ManageData />
       <ListRender />
+      <ConditionalRender/>
+      <ShowUserName name={userName} />
     </div>
   );
 }
