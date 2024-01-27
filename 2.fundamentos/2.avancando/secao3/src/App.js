073bd5126390a349700924f1876c682064773785
Flávio Félix <fau_felix30@hotmail.com>
2024-01-26 21:10:20 -0300

    Feito aulas sobre componentes

--- 2.fundamentos/2.avancando/secao3/src/App.js
+++ 2.fundamentos/2.avancando/secao3/src/App.js
@@ -1,30 +1,55 @@
-import { useState } from 'react';
-import './App.css';
-import City from './assets/city.jpg';
-import ConditionalRender from './components/ConditionalRender';
-import ListRender from './components/ListRender';
-import ManageData from './components/ManageData';
-import ShowUserName from './components/ShowUserName';
+import { useState } from "react";
+import "./App.css";
+import City from "./assets/city.jpg";
+import ConditionalRender from "./components/ConditionalRender";
+import ListRender from "./components/ListRender";
+import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
+import CarDetails from "./components/CarDetails";
+import Fragment from "./components/Fragment";
 
 function App() {
   // const name = "Joaquim";
   const [userName] = useState("Maria");
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0 },
+    { id: 2, brand: "Kia", color: "Branco", newCar: false, km: 34434 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234 },
+  ];
 
   return (
     <div className="App">
       <h1>Avançando no React</h1>
       {/** Imagem em Public */}
       <div>
-        <img src='/img1.jpg' alt='Paisagem'/>
+        <img src="/img1.jpg" alt="Paisagem" />
       </div>
       {/** Imagem em asset */}
       <div>
-        <img src={City} alt='Cidade'/>
+        <img src={City} alt="Cidade" />
       </div>
       <ManageData />
       <ListRender />
-      <ConditionalRender/>
+      <ConditionalRender />
+      {/* Props */}
       <ShowUserName name={userName} />
+      {/* Destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
+      {/* Reaproveitamento */}
+      <CarDetails brand="Ford" km={0} color="Vermelho" newCar={true} />
+      <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
+      {/* Loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/* Fragment */}
+      <Fragment propFragment="Teste" />
     </div>
   );
 }
